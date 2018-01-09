72d58401890c684abb15db788e1fd03739cf2be1
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-01-09 21:08:18 +0000

    Link to add new chore when no chores exist.

--- test/acceptance/chore_index_test.exs
+++ test/acceptance/chore_index_test.exs
@@ -6,4 +6,9 @@ defmodule Tergeo.Acceptance.IndexTest do
     find_element(:class, "chores-list")
   end
 
+  test "Link to add chores is shown when no chores have been added" do
+    navigate_to("/chores")
+    find_element(:class, "add__chore")
+  end
+
 end
