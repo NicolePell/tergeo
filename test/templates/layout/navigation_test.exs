b363e4eabfb1c2bee96235c796fad534846223cf
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-01-30 18:48:03 +0000

    Add a link to chores index page.

--- test/templates/layout/navigation_test.exs
+++ test/templates/layout/navigation_test.exs
@@ -1,10 +1,17 @@
 defmodule TergeoWeb.NavigationTest do
   use TergeoWeb.ConnCase
 
-  @tag :current
   test "shows a 'Sign in with Google' link when not signed in" do
     conn = get build_conn(), "/"
 
     assert html_response(conn, 200) =~ "Sign in with Google"
   end
+
+  @tag :current
+  test "shows a link to the chores index" do
+    conn = get build_conn(), "/"
+
+    assert html_response(conn, 200) =~ "<a href=\"/chores\">Chores</a>"
+  end
+  
 end
